5652ccc3db777f395bfd673a98156bac5debe77c
Abdul Qari <qarix3@gmail.com>
2020-11-24 03:01:53 +0800

    Remove files

--- config/controllers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'app.controller'  => App\Controller\AppController::class,
-    'auth.controller' => App\Controller\AuthController::class,
-    'customer.controller' => App\Controller\CustomerController::class,
-    'product.controller' => App\Controller\ProductController::class,
-];
